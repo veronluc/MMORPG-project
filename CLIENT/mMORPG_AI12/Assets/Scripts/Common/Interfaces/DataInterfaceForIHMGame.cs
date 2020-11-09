4d4ef66fd452b3e95260a525fc8fecb7cb24de69
Grégoire <gregoire@brusketa.com>
2020-11-09 22:45:24 +0100

    Add documentation to server data interfaces

--- CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/DataInterfaceForIHMGame.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/DataInterfaceForIHMGame.cs
@@ -5,9 +5,40 @@ using AI12_DataObjects;
 
 public interface DataInterfaceForIHMGame
 {
+    /// <summary>
+    /// Send a message to the chat
+    /// </summary>
+    /// <param name="idWorld">Identifier of the current world instance</param>
+    /// <param name="text">Message content</param>
     void SendMessage(string idWorld, string text);
+
+    /// <summary>
+    /// Make an action
+    /// </summary>
+    /// <param name="action">Instance of the action</param>
     void MakeAction(Action action);
+
+    /// <summary>
+    /// Get the current connected user
+    /// </summary>
+    /// <returns>
+    /// Current user object
+    /// </returns>
     User GetCurrentUser();
+
+    /// <summary>
+    /// Get the current play
+    /// </summary>
+    /// <returns>
+    /// Current player object
+    /// </returns>
     Player GetCurrentPlayer();
+
+    /// <summary>
+    /// Get the current world
+    /// </summary>
+    /// <returns>
+    /// Current world object
+    /// </returns>
     World GetCurrentWorld();
 }
--- CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/DataInterfaceForNetwork.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/DataInterfaceForNetwork.cs
@@ -5,8 +5,76 @@ using AI12_DataObjects;
 
 public interface DataInterfaceForNetwork
 {
+    /// <summary>
+    /// Receive a list of World instances (used for new or update)
+    /// </summary>
+    /// <param name="worlds">List of worlds</param>
+    void ReceiveListWorlds(List<World> worlds);
+
+    /// <summary>
+    /// Receive a list of User instances (used for new or update)
+    /// </summary>
+    /// <param name="users">List of users</param>
+    void ReceiveListUsers(List<User> users);
+
+    /// <summary>
+    /// Receive a list of User instances of a specific World (used for new or update)
+    /// </summary>
+    /// <param name="users">List of users</param>
+    /// <param name="world">World instance</param>
+    void ReceiveListUsersFromWorld(List<User> users, World world);
+
+    /// <summary>
+    /// Receive a message
+    /// </summary>
+    /// <param name="message">Message content</param>
     void ReceiveMessage(Message message);
+
+    /// <summary>
+    /// Receive an action (player)
+    /// </summary>
+    /// <param name="action">Action to perform</param>
+    /// <param name="player">Player impacted by the action</param>
     void ReceiveAction(Action action, Player player);
-    void ReceiveLoadedWorld(World world);
-    void ReceiveGameState(GameState state);
+
+    /// <summary>
+    /// Receive an action (monster)
+    /// </summary>
+    /// <param name="action">Action to perform</param>
+    /// <param name="monster">Monster impacted by the action</param>
+    void ReceiveAction(Action action, Monster monster);
+
+    /// <summary>
+    /// Receive a world instance
+    /// </summary>
+    /// <param name="world">World instance</param>
+    void ReceiveWorld(World world);
+
+    /// <summary>
+    /// Receive an user instance
+    /// </summary>
+    /// <param name="user">User instance</param>
+    void ReceiveUser(User user);
+
+    /// <summary>
+    /// Server disctonnection (normal)
+    /// </summary>
+    void DisconnectServerStop();
+
+    /// <summary>
+    /// Server disctonnection (error)
+    /// </summary>
+    void DisconnectServerError();
+
+    /// <summary>
+    /// User disconnected from the World it belongs
+    /// </summary>
+    /// <param name="user">User instance</param>
+    void UserDisconnectedFromWorld(User user);
+
+    /// <summary>
+    /// User disconnected from the server it is connected to
+    /// </summary>
+    /// <param name="user">User instance</param>
+    void UserDisconnectedFromServer(User user);
 }
