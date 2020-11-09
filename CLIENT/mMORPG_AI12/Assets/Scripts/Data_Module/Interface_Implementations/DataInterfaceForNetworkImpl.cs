e135a5506315f271b6d058e01a5661ba87b70587
Grégoire <gregoire@brusketa.com>
2020-11-09 22:59:49 +0100

    Implement interfaces

--- CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Interface_Implementations/DataInterfaceForNetworkImpl.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Interface_Implementations/DataInterfaceForNetworkImpl.cs
@@ -5,8 +5,16 @@ using AI12_DataObjects;
 
 public class DataInterfaceForNetworkImpl : DataInterfaceForNetwork
 {
-    public void ReceiveMessage(Message message){}
-    public void ReceiveAction(Action action, Player player){}
-    public void ReceiveLoadedWorld(World world){}
-    public void ReceiveGameState(GameState state){}
+    public void ReceiveListWorlds(List<World> worlds) { }
+    public void ReceiveListUsers(List<User> users) { }
+    public void ReceiveListUsersFromWorld(List<User> users, World world) { }
+    public void ReceiveMessage(Message message) { }
+    public void ReceiveAction(Action action, Player player) { }
+    public void ReceiveAction(Action action, Monster monster) { }
+    public void ReceiveWorld(World world) { }
+    public void ReceiveUser(User user) { }
+    public void DisconnectServerStop() { }
+    public void DisconnectServerError() { }
+    public void UserDisconnectedFromWorld(User user) { }
+    public void UserDisconnectedFromServer(User user) { }
 }
