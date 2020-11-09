2cc605c33e52dae23141e10fb5b9717d964a7a6f
Luc Véron <veron.luc@gmail.com>
2020-11-09 16:30:56 +0100

    fix build issues

--- CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/IHMGameInterface.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/IHMGameInterface.cs
@@ -2,41 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IHMGameInterface
-{
-    public interface IHMGameInterfaceForData
-    {
-
+public interface IHMGameInterface {
         /// <summary>
         /// Launch the game. Start the display of the game view
         /// </summary>
-        public void launch_game();
+        void launch_game();
 
         /// <summary>
         /// Diplays the message sent by the Data module in the chat
         /// </summary>
         /// <param name="message">New message to display</param>
-        public void display_Message();
+        void display_Message();
 
         /// <summary>
         /// Update the display of the user. It will ask to Data module the new position of each entities and their caracteristics (life point, mana point...)
         /// </summary>
-        public void update_Display();
+        void update_Display();
 
         /// <summary>
         /// Notifies the actual user that it can do an action. It means he can use a skill (if he has not already do it) or move (if he still have movement point)
         /// </summary>
-        public void notify_action_possible();
+        void notify_action_possible();
 
         /// <summary>
         /// Displays on user's screen that the server has stopped
         /// </summary>
-        public void display_Server_Stop();
+        void display_Server_Stop();
 
         /// <summary>
         /// Displays on user's screen that the user has logout 
         /// </summary>
         /// <param name="user">user that just log out</param>
-        public void display_User_Logout();
-    }
+        void display_User_Logout();
 }
