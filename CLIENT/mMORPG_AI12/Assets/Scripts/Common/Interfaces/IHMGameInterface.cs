0eeb34e38e7ff50828c3e4abc538d7647a0adf90
DorianZielinski <dorian.zielinski@etu.utc.fr>
2020-11-09 10:32:17 +0100

    Init IHMGameInterface
    Initialization IHMGameInterface and IHMGameInterfaceImpl

--- CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/IHMGameInterface.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/IHMGameInterface.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public interface IHMGameInterface
 {
-    
+    public interface IHMGameInterfaceForData
+    {
+
+        /// <summary>
+        /// Diplays the message sent by the Data module in the chat
+        /// </summary>
+        /// <param name="message">New message to display</param>
+        public void display_Message();
+
+        /// <summary>
+        /// Update the display of the user. It will ask to Data module the new position of each entities and their caracteristics (life point, mana point...)
+        /// </summary>
+        public void update_Display();
+
+        /// <summary>
+        /// Notifies the actual user that it can do an action. It means he can use a skill (if he has not already do it) or move (if he still have movement point)
+        /// </summary>
+        public void notify_action_possible();
+
+        /// <summary>
+        /// Displays on user's screen that the server has stopped
+        /// </summary>
+        public void display_Server_Stop();
+
+        /// <summary>
+        /// Displays on user's screen that the user has logout 
+        /// </summary>
+        /// <param name="user">user that just log out</param>
+        public void display_User_Logout();
+    }
 }
