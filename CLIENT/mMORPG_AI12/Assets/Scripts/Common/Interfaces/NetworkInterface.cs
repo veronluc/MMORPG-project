baa1616bd9cad97536709145be925e1fef0ccaae
Yann Kerkhof <yann.kerkhof@gmail.com>
2020-11-09 17:43:41 +0100

    Add network interfaces for data.

--- CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/NetworkInterface.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Common/Interfaces/NetworkInterface.cs
@@ -4,5 +4,63 @@ using UnityEngine;
 
 public interface NetworkInterface
 {
-    
+    /// <summary>
+    /// Connecter le client au serveur.
+    /// </summary>
+    /// <param name="user">Informations sur l'utilisateur SANS SON ID (géré par le réseau)</param>
+    /// <param name="ipAdress">Adresse IP du serveur</param>
+    /// <param name="port">Port de connexion du serveur</param>
+    void ConnectUser(User user, string ipAdress, int port);
+
+    /// <summary>
+    /// Envoyer un messag
+    /// </summary>e au serveur (le client connait l'id du user à envoyer au serveur)
+    /// <param name="message">Message à envoyer</param>
+    void SendChatMessage(Message message);
+
+    /// <summary>
+    /// Envoyer une action au serveur. Le client connait l'id du user à envoyer au serveur, et le serveur fait le lien entre le user et le monde auquel appartient le joueur
+    /// </summary>
+    /// <param name="player">Personnage en jeu</param>
+    /// <param name="action">Action à effectuer</param>
+    void SendAction(Player player, Action action);
+
+    /// <summary>
+    /// Envoyer un nouveau monde (world) au serveur
+    /// </summary>
+    /// <param name="world"></param>
+    void AddNewWorld(World world);
+
+    /// <summary>
+    /// Connecter le joueur à un monde du serveur
+    /// </summary>
+    /// <param name="idWorld">Monde auquel le joueur doit se connecter</param>
+    void ConnectToWorld(int idWorld);
+
+    /// <summary>
+    /// Envoyer une demande de la liste de tous les utilisateurs connectés du serveur
+    /// </summary>
+    void AskServerUserList();
+
+    /// <summary>
+    /// Envoyer une demande de la liste de tous les utilisateurs mondes du serveur
+    /// </summary>
+    void AskServerWorldList();
+
+    /// <summary>
+    /// Actualiser les infos de l'utilisateur et les envoyer au serveur (TOUJOURS SANS l'ID, géré par le réseau)
+    /// </summary>
+    /// <param name="user"></param>
+    void RefreshUserInfos(User user);
+
+    /// <summary>
+    /// Envoyer une demande de deconnexion du user au monde
+    /// </summary>
+    void DisconnectUserFromWorld();
+
+    /// <summary>
+    /// Envoyer une demande de deconnexion du user au serveur
+    /// </summary>
+    void DisconnectUserFromServer();
+
 }
---
+++ SERVER/Interfaces/NetworkInterface.cs
@@ -0,0 +1,75 @@
+﻿using System;
+public interface NetworkInterface
+{
+    /// <summary>
+    /// Envoyer une liste de worlds à un utilisateur user.
+    /// </summary>
+    /// <param name="user">Utilisateur de destination</param>
+    /// <param name="worlds">Liste de worlds</param>
+    void SendWorldsList(User user, List<World> worlds);
+
+    /// <summary>
+    /// Envoyer une liste d'utilisateurs à un utilisateur user.
+    /// </summary>
+    /// <param name="user">Utilisateur de destination</param>
+    /// <param name="users">Liste de users à envoyer</param>
+    void SendUsersList(User user, List<User> users);
+
+    /// <summary>
+    /// Envoyer une liste d'utilisateurs connectés à un monde à un utilisateur user.
+    /// </summary>
+    /// <param name="user">Utilisateur de destination</param>
+    /// <param name="users">Liste des utilisateurs connectés au monde world </param>
+    /// <param name="world">Monde sur lequel sont connectés les utilisateurs de la liste</param>
+    void SendUsersListFromWorld(User user, List<User> users, World world);
+
+    /// <summary>
+    /// Envoyer un message à un client (user)
+    /// </summary>
+    /// <param name="user">User de destination</param>
+    /// <param name="message">Message à envoyer</param>
+    void SendMessageToUser(User user, Message message);
+
+    /// <summary>
+    /// Envoyer une action d'un personnage (player) à un client (user)
+    /// </summary>
+    /// <param name="user">User de destination</param>
+    /// <param name="action">Action effectué</param>
+    /// <param name="player">Personnage de la partie qui a effectué l'action</param>
+    void SendActionToUser(User user, Action action, Player player);
+
+    /// <summary>
+    /// Envoyer une action d'un monstre (monster) à un client (user)
+    /// </summary>
+    /// <param name="user">User de destination</param>
+    /// <param name="action">Action effectué</param>
+    /// <param name="monster">Monstre de la partie qui a effectué l'action</param>
+    void SendActionToUser(User user, Action action, Monster monster);
+
+    /// <summary>
+    /// Envoyer le résultat de la demande de connexion d'un user à un monde
+    /// </summary>
+    /// <param name="user">User de destination</param>
+    /// <param name="world">Monde auquel il se connecte</param>
+    /// <param name="result">Resultat de connexion</param>
+    /// <param name="message">Message du resultat</param>
+    void SendConfirmationUserConnectionToWorld(User user, World world, bool result, string message);
+
+    /// <summary>
+    /// Envoyer un message au user User comme quoi le serveur va s'arreter
+    /// </summary>
+    /// <param name="user">User de destination</param>
+    void SendStopServer(User user);
+
+    /// <summary>
+    /// Envoyer un message à userDestination que le user userDisconnected s'est deconnecté
+    /// </summary>
+    /// <param name="user"></param>
+    void SendUserDisconnectedWorld(User userDestination, User userDisconnected);
+
+    /// <summary>
+    /// Envoyer un message à userDestination que le user userDisconnected s'est deconnecté du server
+    /// </summary>
+    /// <param name="user"></param>
+    void SendUserDisconnectedServer(User userDestination, User userDisconnected);
+}
