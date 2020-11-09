1613c8e2f4106ee7c64973ab3f32eb497a95176e
DorianZielinski <dorian.zielinski@etu.utc.fr>
2020-11-09 18:59:55 +0100

    Init SkillButton
    Add the skillButton and link the to a script

---
+++ CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Game_Module/HUD_Scripts/SkillController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillController : MonoBehaviour
+{
+
+    private Text buttonText;
+
+    // Test button click
+    public void UseSkill(int skillNumber)
+    {
+        //buttonText = GetComponentInChildren<Text>();
+        //string buttonName = "ButtonSkill_" + skillNumber;
+        //Debug.Log(buttonName);
+        //buttonText = GameObject.Find(buttonName).GetComponent<Text>();
+        buttonText = GameObject.Find("ButtonSkill_" + skillNumber).GetComponentInChildren<Text>();
+        buttonText.text = "This skill's number is n°" + skillNumber;
+    }
+}
