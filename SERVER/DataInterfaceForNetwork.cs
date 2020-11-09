4d2b52faf9d0a9f7f37d05b0c2ac92b6e6c349b2
Arthur LEFEVRE <lefevre.arthur@outlook.fr>
2020-11-09 23:08:21 +0100

    DataInterfaceForNetwork + classes server

---
+++ SERVER/DataInterfaceForNetwork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AI12_DataObjects;
+public interface DataInterfaceForNetwork
+{
+    List<User> GetUsersFromWorld(World world);
+    GameState MakeAction(Action action, World world, Player player);
+    Player GetNextPlayer(World world);
+    void LoadWorld(World world);
+    void UnloadWorld(World world);
+    List<World> GetWorlds();
+    World GetWorldDetails(string idWorld);
+    (List<User>, List<World>) ConnectUser(User user);
+    void DisconnectUser();
+    List<User> GetConnectedUsers();
+    User GetUserData(string userId);
+    void SendChatMessage(World world, Message message, Player player);
+}
