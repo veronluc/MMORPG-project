884ce60e79c7a1033a5b2e6c47345e3b0799f67c
Anais Mace <anais.mace@etu.utc.fr>
2020-11-10 17:17:48 +0100

    comment objects that are not currently available

--- CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Main_Module/MainConnectedScreen.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Main_Module/MainConnectedScreen.cs
@@ -3,48 +3,70 @@ using UnityEngine;
 
 public class MainConnectedScreen : MonoBehaviour
 {
-    // Proprietes
+    // Properties
     private string serverIp;
     private string port;
-    private List<User> usersList; // TODO : retrieve User object when available
-    private List<World> worldsList;
+    // private List<User> usersList; // TODO : retrieve User object when available
+    // private List<World> worldsList; // TODO : retrieve World object when available
 
-    // Ecrans
-    private ConnexionScreen connexionScreen; // TODO : develop class
-    private ConnectToAServerScreen connectToAServerScreen; // TODO : develop class
-    private ManageMyWorldsScreen manageMyWorldsScreen; // TODO : develop class
-    private ManageMyCharactersScreen manageMyCharactersScreen; // TODO : develop class
+    // Screens
+    // private ConnexionScreen connexionScreen; // TODO : develop class
+    // private ConnectToAServerScreen connectToAServerScreen; // TODO : develop class
+    // private ManageMyWorldsScreen manageMyWorldsScreen; // TODO : develop class
+    // private ManageMyCharactersScreen manageMyCharactersScreen; // TODO : develop class
 
     public void Awake()
     {
         // TODO : when classes available : add parameters if needed
-        connexionScreen = new ConnexionScreen();
-        connectToAServerScreen = new ConnectToAServerScreen();
-        manageMyWorldsScreen = new ManageMyWorldsScreen();
-        manageMyCharactersScreen = new ManageMyCharactersScreen();
+        // connexionScreen = new ConnexionScreen();
+        // connectToAServerScreen = new ConnectToAServerScreen();
+        // manageMyWorldsScreen = new ManageMyWorldsScreen();
+        // manageMyCharactersScreen = new ManageMyCharactersScreen();
     }
 
     public void Start() { }
 
-    public void DisplayCreateProfileScreen() { }
+    public void DisplayCreateProfileScreen() {
+        throw new System.NotImplementedException();
+    }
 
-    public void UpdateListDisplay(List<User> listUsers) { }
+    public void UpdateListDisplay(/*List<User> listUsers*/) {
+        throw new System.NotImplementedException();
+    }
 
-    private void OnClickConnectToAServer() { }
+    private void OnClickConnectToAServer() {
+        throw new System.NotImplementedException();
+    }
 
-    private void OnClickManageMyWorlds() { }
+    private void OnClickManageMyWorlds() {
+        throw new System.NotImplementedException();
+    }
 
-    private void OnClickManageMyCharacters() { }
+    private void OnClickManageMyCharacters() {
+        throw new System.NotImplementedException();
+    }
 
-    private void OnClickOneUser() { }
+    private void OnClickOneUser() {
+        throw new System.NotImplementedException();
+    }
 
-    private void OnClickOneWorld() { }
+    private void OnClickOneWorld() {
+        throw new System.NotImplementedException();
+    }
 
-    private void OnClickJoin() { }
+    private void OnClickJoin() {
+        throw new System.NotImplementedException();
+    }
 
-    private void OnClickLogout() { }
+    private void OnClickLogout() {
+        throw new System.NotImplementedException();
+    }
 
-    private void ConnectWorld(string worldIpAdress) { }
+    private void ConnectWorld(string worldIpAdress) {
+        throw new System.NotImplementedException();
+    }
 
-    private void ChangeScene(GameBoard gameBoard) { }
+    private void ChangeScene(/*GameBoard gameBoard*/) {
+        throw new System.NotImplementedException();
+    }
 }
