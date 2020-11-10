0521695110a477c60cb6e7464a79afa327ae8418
Maxime Gaillard <maxime.gaillard@etu.utc.fr>
2020-11-10 18:17:55 +0100

    Modify end of line settings

--- CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Main_Module/Interface_implementation/IHMMainInterfaceImpl.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Main_Module/Interface_implementation/IHMMainInterfaceImpl.cs
@@ -1,1 +1,59 @@
-﻿using System.Collections;using System.Collections.Generic;using System.ComponentModel;using UnityEngine;using AI12_DataObjects;public class IHMMainInterfaceImpl : IHMMainInterface{    public void LoadMainScene()    {        throw new System.NotImplementedException();    }    public void DisplayListUsersWorlds(List<User> usersList, List<World> worldsList)    {        throw new System.NotImplementedException();    }    public void DisplayNewAvailableWorld(World newWorld)    {        throw new System.NotImplementedException();    }    public void DisplayNewConnectedUser(User newUser)    {        throw new System.NotImplementedException();    }    public void DisplayUserDetail(string token)    {        throw new System.NotImplementedException();    }    public void DisplayWorldDetail(World world)    {        throw new System.NotImplementedException();    }    public void IHMLogOutConfirmation()    {        throw new System.NotImplementedException();    }    public void DisplayOwnerDisconnected(User user)    {        throw new System.NotImplementedException();    }    public void DisplayUserLogOut(User user)    {        throw new System.NotImplementedException();    }    public void ServerStopped()    {        throw new System.NotImplementedException();    }}
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+using AI12_DataObjects;
+
+public class IHMMainInterfaceImpl : IHMMainInterface
+{
+    public void LoadMainScene()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DisplayListUsersWorlds(List<User> usersList, List<World> worldsList)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DisplayNewAvailableWorld(World newWorld)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DisplayNewConnectedUser(User newUser)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DisplayUserDetail(string token)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DisplayWorldDetail(World world)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void IHMLogOutConfirmation()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DisplayOwnerDisconnected(User user)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DisplayUserLogOut(User user)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ServerStopped()
+    {
+        throw new System.NotImplementedException();
+    }
+}
