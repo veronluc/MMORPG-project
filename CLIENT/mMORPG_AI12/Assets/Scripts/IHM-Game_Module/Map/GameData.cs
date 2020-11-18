967724ee5631e75bb984fa238d12621ad5fa0a81
Guillaume Nibert <guillaume.nibert@etu.utc.fr>
2020-11-18 17:49:20 +0100

    Init dynamic map

---
+++ CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Game_Module/Map/GameData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public string Size = "7x6"; 
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Game_Module/Map/TilesHolder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class TilesHolder : MonoBehaviour
+{
+    private Tile _baseTile; private void Awake()
+    {
+        _baseTile = (Tile)AssetDatabase.LoadAssetAtPath("Assets/Materials/IHM-Game_Module/Palettes/Overworld/overworld_53.asset", typeof(Tile));
+    }
+    public Tile GetBaseTile()
+    {
+        return _baseTile;
+    }
+}
