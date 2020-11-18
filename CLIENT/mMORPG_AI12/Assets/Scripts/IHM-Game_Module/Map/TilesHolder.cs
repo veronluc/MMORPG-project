4a85bb8047d774ea1610e9eecf07202b94c28f80
Guillaume Nibert <guillaume.nibert@etu.utc.fr>
2020-11-18 17:49:20 +0100

    Init dynamic map

--- CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Game_Module/Map/TilesHolder.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Game_Module/Map/TilesHolder.cs
@@ -1,19 +1,14 @@
-﻿#if UNITY_EDITOR
-    using UnityEditor;
-#endif
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-
+using UnityEditor;
 
 public class TilesHolder : MonoBehaviour
 {
     private Tile _baseTile; private void Awake()
     {
-#if UNITY_EDITOR
         _baseTile = (Tile)AssetDatabase.LoadAssetAtPath("Assets/Materials/IHM-Game_Module/Palettes/Overworld/overworld_53.asset", typeof(Tile));
-#endif
     }
     public Tile GetBaseTile()
     {
