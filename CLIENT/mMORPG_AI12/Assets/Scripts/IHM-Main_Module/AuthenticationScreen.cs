2c328a27f621abbb6c7bd684c52683ea03a51803
Julien Revaud <julien.revaud@etu.utc.fr>
2020-11-25 19:27:11 +0100

    [WIP] add main scripts to the screen (no global screens manager and no profile creation screen)

---
+++ CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Main_Module/AuthenticationScreen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class AuthenticationScreen : MonoBehaviour
+{
+    // Properties
+    private IHMMainModule ihmMainModule;
+    private DataInterfaceForIHMMain dataInterface;
+
+    public void Awake()
+    {
+        this.ihmMainModule = GameObject.FindGameObjectWithTag("IHMMainModule").GetComponent<IHMMainModule>();
+        this.dataInterface = ihmMainModule.dataInterface;
+    }
+
+    public void Start()
+    {
+        this.dataInterface = ihmMainModule.dataInterface;
+    }
+
+    /// <summary>
+    /// Create a local session for the user
+    /// If the user was connected to a server during its last session,
+    /// the client connects directly to this server
+    /// </summary>
+    /// <param name="login">User pseudo</param>
+    /// <param name="password">User password</param>
+    public void UserLogIn(string login, string password)
+    {
+        Debug.Log("2 : " + login);
+        Debug.Log("2 : " + password);
+        try
+        {
+            if (!login.Equals("") || !password.Equals(""))
+            {
+                dataInterface.CreateUserSession(login, password);
+            }
+            else
+            {
+                MessagePopupManager.ShowWarningMessage("Vous n'avez pas entré votre login ou votre mot de passe");
+            }
+        }
+        catch (Exception e)
+        {
+            // handle user log in error 
+        }
+    }
+}
---
+++ CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Main_Module/AuthenticationScreen/LocalAuthenticationManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalAuthenticationManager : MonoBehaviour
+{
+    private string login;
+    private string password;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        this.login = "";
+        this.password = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetLogin(string login)
+    {
+        this.login = login;
+    }
+
+    public void SetPassword(string password)
+    {
+        this.password = password;
+    }
+
+    public void ClickOnLogIn()
+    {
+        Debug.Log(this.login);
+        Debug.Log(this.password);
+        //Call the AuthenticationScreen (script) function to discuss with other modules
+        GameObject.FindGameObjectWithTag("IHMMainModule").GetComponent<AuthenticationScreen>().UserLogIn(login,password);
+    }
+}
