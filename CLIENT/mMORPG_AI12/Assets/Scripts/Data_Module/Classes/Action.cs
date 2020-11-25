ce391b4d9ade7fff0e3f01944f10c271860db718
Grégoire <gregoire.thibo@gmail.com>
2020-11-25 10:33:28 +0100

    make action class abstract

--- CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Classes/Action.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Classes/Action.cs
@@ -4,8 +4,7 @@ using System;
 
 namespace AI12_DataObjects {
     [Serializable()]
-    // TODO est-ce que abstrait ?
-    public class Action
+    public abstract class Action
     {
         public Player player { get; set; }
         public World world { get; set; }
