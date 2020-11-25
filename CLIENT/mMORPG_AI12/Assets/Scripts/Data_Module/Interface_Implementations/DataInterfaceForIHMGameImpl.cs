17d21b2e746061437518311a43423e78e08d2aa0
Grégoire <gregoire.thibo@gmail.com>
2020-11-25 08:56:04 +0100

    add send action to server

--- CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Interface_Implementations/DataInterfaceForIHMGameImpl.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Interface_Implementations/DataInterfaceForIHMGameImpl.cs
@@ -5,10 +5,18 @@ using AI12_DataObjects;
 
 public class DataInterfaceForIHMGameImpl : DataInterfaceForIHMGame
 {
+    public DataInterfaceForIHMGameImpl()
+    {
+    }
+
     public void SendMessage(Message message) {
         DataModule.networkInterface.SendChatMessage(message);
     }
-    public void MakeAction(Action action) { }
+
+    public void MakeAction(Action action) {
+        DataModule.networkInterface.SendAction(action.player, action);
+    }
+
     public User GetCurrentUser() { return null; }
     public Player GetCurrentPlayer() { return null; }
     public World GetCurrentWorld() { return null; }
