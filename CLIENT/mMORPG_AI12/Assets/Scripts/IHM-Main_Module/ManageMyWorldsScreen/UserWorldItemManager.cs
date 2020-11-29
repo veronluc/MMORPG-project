f92a69c527ad48bdb1bdc5784fe73ee7b8663e04
Mace <anais.mace@etu.utc.fr>
2020-11-29 15:48:08 +0100

    add prefab for small info of the world + add managers

---
+++ CLIENT/mMORPG_AI12/Assets/Scripts/IHM-Main_Module/ManageMyWorldsScreen/UserWorldItemManager.cs
@@ -0,0 +1,40 @@
+﻿using AI12_DataObjects;
+using TMPro;
+using UnityEngine;
+
+public class UserWorldItemManager : MonoBehaviour
+{
+    public TextMeshProUGUI worldName;
+    public TextMeshProUGUI worldSize;
+    public TextMeshProUGUI worldType;
+
+    void Start()
+    {
+    }
+
+    /// <summary>
+    /// Set some world info. to this gameObject (to show it on the screen)
+    /// </summary>
+    /// <param name="world">The world that will be displayed in this gameObject</param>
+    public void SetWorldInfoToGameObject(World world)
+    {
+        this.worldName.text = world.name;
+
+        this.worldSize.text = world.sizeMap.ToString();
+
+        this.worldType.text = world.gameMode.ToString().ToUpper();
+    }
+    
+    /// <summary>
+    /// Only for Test purpose
+    /// </summary>
+    public void SetRandomInfoToGameObject()
+    {
+        this.worldName.text = "A wonderful world name";
+
+        this.worldSize.text = "SMALL";
+
+        this.worldType.text = "PVP";
+    }
+    
+}
