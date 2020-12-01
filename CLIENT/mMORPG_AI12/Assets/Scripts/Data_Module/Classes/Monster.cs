6eb22b93661401186d46bccdeba5458a6e33e605
Arthur LEFEVRE <lefevre.arthur@outlook.fr>
2020-12-01 10:46:41 +0100

    Generate GameState on the server end on World reception

--- CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Classes/Monster.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Classes/Monster.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AI12_DataObjects
 {
+    public enum MonsterTypes
+    {
+        Goblin,
+        Sorcerer
+    }
+
     [Serializable()]
     public class Monster : Entity
     {
@@ -14,6 +21,93 @@ namespace AI12_DataObjects
             this.visionLength = visionLength;
             this.dropGold = dropGold;
             this.dropXp = dropXp;
+        }
+
+        public static Monster GetMonsterFromType(MonsterTypes type, string name, Location location)
+        {
+            Random r = new Random();
+            Monster m;
+            switch(type)
+            {
+                case MonsterTypes.Goblin:
+                    m = new Monster(
+                        "Goblin " + name,
+                        1, // level
+                        5, // vitalityMax
+                        5, // vitality
+                        0, // manaMax
+                        0, // mana
+                        3, // strength
+                        0, // intelligence
+                        4, // defense
+                        3, // PM
+                        location,
+                        new EntityClass(
+                            "Goblin " + name,
+                            5, //baseVitality
+                            0, //baseMana
+                            3, //baseStrength
+                            0, //baseIntelligence
+                            4, //baseDefense
+                            3, //basePM
+                            Entities.monster,
+                            new List<Skill>() {
+                                new Skill(
+                                    "CAC Attack",
+                                    1, // Zone
+                                    1 + 3, // Att Damage (should be 1 + strength)
+                                    0, //Mana
+                                    false //Healing
+                                )
+                            }
+                        ),
+                        10, // Vision Length
+                        r.Next(20), // Gold Drop
+                        r.Next(100) // Exp Drop
+                    );
+                    break;
+                case MonsterTypes.Sorcerer:
+                    m = new Monster(
+                        "Sorcerer " + name,
+                        1, // level
+                        5, // vitalityMax
+                        5, // vitality
+                        5, // manaMax
+                        5, // mana
+                        0, // strength
+                        3, // intelligence
+                        2, // defense
+                        2, // PM
+                        location,
+                        new EntityClass(
+                            "Sorcerer " + name,
+                            5, //baseVitality
+                            5, //baseMana
+                            0, //baseStrength
+                            3, //baseIntelligence
+                            2, //baseDefense
+                            2, //basePM
+                            Entities.monster,
+                            new List<Skill>() {
+                                new Skill(
+                                    "Range Attack",
+                                    4, // Zone
+                                    1 + 3, // Att Damage (should be 1 + intelligence)
+                                    0, //Mana
+                                    false //Healing
+                                )
+                            }
+                        ),
+                        50, // Vision Length
+                        r.Next(50), // Gold Drop
+                        r.Next(250) // Exp Drop
+                    );
+                    break;
+                default:
+                    m = GetMonsterFromType(MonsterTypes.Goblin, name, location);
+                    break;
+            }
+            return m;
         }
     }
 }
