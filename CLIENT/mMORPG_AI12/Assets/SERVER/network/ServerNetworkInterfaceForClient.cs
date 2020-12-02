67935996497f0ce5083b3ade6fb00b74443ffbf6
Theo Ducatez <theoducatez05@gmail.com>
2020-12-02 18:34:52 +0100

    fix server documentation

--- CLIENT/mMORPG_AI12/Assets/SERVER/network/ServerNetworkInterfaceForClient.cs
+++ CLIENT/mMORPG_AI12/Assets/SERVER/network/ServerNetworkInterfaceForClient.cs
@@ -13,14 +13,14 @@ public interface ServerNetworkInterfaceForClient
     /// </summary>
     /// <param name="user">The user we want to send the list to</param>
     /// <param name="worlds">The list of worlds we want to send</param>
-    public void SendWorldsList(User user, List<World> worlds);
+    void SendWorldsList(User user, List<World> worlds);
 
     /// <summary>
     /// Sends a list of users to the client (the given user)
     /// </summary>
     /// <param name="user">The user we want to send the list to</param>
     /// <param name="users">The user list we want to send</param>
-    public void SendUsersList(User user, List<User> users);
+    void SendUsersList(User user, List<User> users);
 
     /// <summary>
     /// Sends a list of users in a specific world world to the client (the given user)
@@ -28,21 +28,21 @@ public interface ServerNetworkInterfaceForClient
     /// <param name="user">The user we want to send the list to</param>
     /// <param name="users">The user list we want to send</param>
     /// <param name="world">The world</param>
-    public void SendUsersListFromWorld(User user, List<User> users, World world);
+    void SendUsersListFromWorld(User user, List<User> users, World world);
 
     /// <summary>
     /// Send a message to the given user client
     /// </summary>
     /// <param name="user">The user we want to send the message to</param>
     /// <param name="message">The message to send</param>
-    public void SendMessageToUser(User user, Message message);
+    void SendMessageToUser(User user, Message message);
 
     /// <summary>
     /// Send an action to the given user
     /// </summary>
     /// <param name="user">The user we want to send to</param>
     /// <param name="gameState">The gamestate with the action being made</param>
-    public void SendActionToUser(User user, GameState gameState);
+    void SendActionToUser(User user, GameState gameState);
 
     /// <summary>
     /// Send a confirmation that the user has connected to a world
@@ -52,27 +52,27 @@ public interface ServerNetworkInterfaceForClient
     /// <param name="player">The player connected</param>
     /// <param name="result">The result of the connection, true is good, false is not good</param>
     /// <param name="message">The message of the result</param>
-    public void SendConfirmationUserConnectionToWorld(User user, World world, Player player, bool result, string message);
+    void SendConfirmationUserConnectionToWorld(User user, World world, Player player, bool result, string message);
 
     /// <summary>
     /// Send a message to the user when the server is about to stop
     /// </summary>
     /// <param name="user">The user we want to send to</param>
-    public void SendStopServer(User user);
+    void SendStopServer(User user);
 
     /// <summary>
     /// Send a message saying that a player has disconnected from a given world
     /// </summary>
     /// <param name="userDestination">The User we want to send to</param>
     /// <param name="userDisconnected">The player who has disconnected</param>
-    public void SendUserDisconnectedWorld(User userDestination, User userDisconnected);
+    void SendUserDisconnectedWorld(User userDestination, User userDisconnected);
 
     /// <summary>
     /// Send a message saying that a player has disconnected from the server
     /// </summary>
     /// <param name="userDestination">The User we want to send to</param>
     /// <param name="userDisconnected">The player who has disconnected</param>
-    public void SendUserDisconnectedServer(User userDestination, User userDisconnected);
+    void SendUserDisconnectedServer(User userDestination, User userDisconnected);
 
     /// <summary>
     /// Send two lists of users and worlds to the user 
@@ -80,12 +80,12 @@ public interface ServerNetworkInterfaceForClient
     /// <param name="user">The user we want to send to</param>
     /// <param name="users">The list of users</param>
     /// <param name="worlds">The list of worlds</param>
-    public void SendListUsersWorlds(User user, List<User> users, List<World> worlds);
+    void SendListUsersWorlds(User user, List<User> users, List<World> worlds);
 
     /// <summary>
     /// Sends a packet to the user represented by its id
     /// </summary>
     /// <param name="idString">The id of the user</param>
     /// <param name="packet">The packet we want to send</param>
-    public void SendPacket(string idString, Packet packet);
+    void SendPacket(string idString, Packet packet);
 }
