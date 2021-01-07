efb69f54c6daf5e58ae0010c9d0a51160ab90639
Grégoire <gregoire.thibo@gmail.com>
2021-01-07 18:05:11 +0100

    add random Player generator

--- CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Classes/Player.cs
+++ CLIENT/mMORPG_AI12/Assets/Scripts/Data_Module/Classes/Player.cs
@@ -2,6 +2,14 @@
 
 namespace AI12_DataObjects
 {
+    public enum PlayerType
+    {
+        Warrior,
+        Rogue,
+        Priest,
+        Mage
+    }
+
     [Serializable()]
     public class Player : Entity
     {
@@ -26,6 +34,56 @@ namespace AI12_DataObjects
         public override bool isMonster()
         {
             return false;
+        }
+
+        public static Player GenerateRandomPlayer()
+        {
+            Random rand = new Random();
+
+            // List of names
+            string[] names = new string[] {
+                "Dominique",
+                "Marie-Hélène",
+                "Valérie",
+                "Ahmed",
+                "Aziz",
+                "Benjamin",
+                "Philipe",
+                "Manuel",
+                "Karine",
+                "Jennifer",
+                "Benoît",
+                "Frédéric",
+                "Antoine",
+                "Sylvain"
+            };
+
+            int randomMaxVitality = rand.Next(1, 100);
+            int randomVitality = rand.Next(1, randomMaxVitality);
+            int randomMaxMana = rand.Next(1, 100);
+            int randomMana = rand.Next(1, randomMaxMana);
+
+            EntityClass warrior = new EntityClass(
+                "Warrior",
+                rand.Next(1,100),
+
+                );
+
+            return new Player(
+                new Guid(),
+                names[rand.Next(0, names.Length)],
+                rand.Next(0, 100),
+                randomMaxVitality,
+                randomVitality,
+                randomMaxMana,
+                randomMana,
+                rand.Next(1, 100),
+                rand.Next(1, 100),
+                rand.Next(1, 100),
+                3,
+                null,
+                
+                );
         }
     }
 }
